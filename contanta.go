08462ec7fa8cbe517ee94f7ef7bd55eccfc84cd1
nden101 <ndenawalludin101@gmail.com>
2022-01-22 02:25:18 +0700

    Variable dan Constanta

---
+++ contanta.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	const (
+		siswa	= "Nden Awalludin"
+		jurusan	= "IPA"
+		nilai	= 95
+	)
+
+	fmt.Println("Nama siswa	:", siswa)
+	fmt.Println("Jurusan		:", jurusan)
+	fmt.Println("Nilai		:", nilai)
+}
---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nama string
+	var thnlhr1 = 1999 
+	thnlhr2 := 2005
+
+	nama = "Nden Awalludin"
+	
+	fmt.Println(nama)
+	fmt.Println(thnlhr1)
+
+	nama = "Bahrul Ulumudin"
+
+	fmt.Println(nama)
+	fmt.Println(thnlhr2)
+}
